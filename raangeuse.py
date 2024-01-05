ad48559f63b72893c71c0b962adb59725c2f465a
kuldeepsinghvns <133557298+kuldeepsinghvns@users.noreply.github.com>
2024-01-05 15:27:27 +0530

    Initial commit

---
+++ raangeuse.py
@@ -0,0 +1,23 @@
+for i in range(5):
+    print(i)
+
+
+for i in range(10, 0, -1):
+    print("reverse", i)
+
+for i in range(1, 11):
+    print(i)
+
+for i in range(1, 11, 2):
+    print("sum", i)
+
+
+for i in range(2, 11, 2):
+    print(i)
+
+
+for i in range(10, 0, -2):
+    print(i)
+
+for i in range(9, 0, -2):
+    print("sum", i)
